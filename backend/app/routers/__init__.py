74a6399db5a008169bca456af5964530e01dd4b0
dulmini20212103-arch <dulmini.20212103@iit.ac.lk>
2026-01-21 09:44:30 +0530

    Add comments

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,4 +1,8 @@
+#router package initializer
+#controls how API route modules are exposed and imported.
+#Each imported router is renamed
 from .auth import router as auth_router
 from .users import router as users_router
 
+#defines what is publicly exposed when someone imports this package
 __all__ = ["auth_router", "users_router"]
